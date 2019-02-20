2694395c602acf9679845e028b0693c7e7a5a863
Obermeyer <moberme@calstatela.edu>
2019-02-20 08:34:05 -0800

    Excel import decoding class finished

--- LabManagement/ImportSchedule.cs
+++ LabManagement/ImportSchedule.cs
@@ -25,59 +25,59 @@ namespace LabManagement
 
         static public void GetExcelSchedule()
         {
-            Regex coursePattern = new Regex(@"EE(\d{4})");
-            Regex coursePattern2 = new Regex(@"([A-Z]{1,4})(\d{4})-?(\d{0,2})");
-            Regex course_Number = new Regex("EE");
+            Regex coursePattern = new Regex(@"([A-Z]{1,4})(\d{4})-?(\d{0,2})");
+            //Regex faculty1Pattern = new Regex(@"^(\w+)\b");
+            //Regex faculty2Pattern = new Regex(@"/(\w+)");
+            Regex facultyPattern = new Regex(@"(\w+)\/?(\w+)?");
+            //Regex timePatternOld = new Regex(@"^(TBA|[MTWRFSU])([MTWRFSU]?)([MTWRFSU]?)([MTWRFSU]?)");
+            Regex timePattern = new Regex(@"^(TBA|[MTWRFSU])([MTWRFSU]?)([MTWRFSU]?)([MTWRFSU]?)\s(\d{3,4})([AP]?M?)-(\d{3,4})([AP]?M?)");
+            //Regex timePattern = new Regex(@"^(TBA|[MTWRFSU]{1,4})\s(\d{3,4})([AP]?M?)-(\d{3,4})([AP]?M?)");
             string fileName = @"C:\Users\moberme\Documents\ArletteTestSchedule.xlsx";
-             ExcelData ws = new ExcelData(fileName, 1);
-
-            //for (int i = 1; i <= ws.rowCount; i++)
-            //{
-            //    for (int j = 1; j <= ws.colCount; j++)
-            //    {
-            //        System.Console.Write(ws.excelArray[i - 1, j - 1] +"_|_");
-            //    }
-            //    System.Console.WriteLine(" ");
-            //}
+            ExcelData ws = new ExcelData(fileName, 1);
 
             Boolean isCourse;
-            string rawCourse;
-            string courseLetters;
-            string courseNumber;
-            string section;
+            string rawCourse, courseLetters, courseNumber, section;
             string title;
             string credit;
-            string faculty;
-            string days;
-            string startTime;
-            string endTime;
+            string rawFaculty, faculty1, faculty2;
+            string rawTime, day1, day2, day3, day4, startTime, startTimeAPM,  endTime, endTimeAPM;
             string room;
+            //MTWR //MWF S  TBA U
 
             for (int currentRow = 0; currentRow <= ws.rowCount - 1; currentRow++)
             {
                 rawCourse = ws.excelArray[currentRow, 0];
-                isCourse = coursePattern2.IsMatch(rawCourse);
-
+                isCourse = coursePattern.IsMatch(rawCourse);
 
                 if (isCourse)
                 {
-                    courseLetters = coursePattern2.Match(rawCourse).Groups[1].Value;
-                    courseNumber = coursePattern2.Match(rawCourse).Groups[2].Value;
-                    section = coursePattern2.Match(rawCourse).Groups[3].Value;
+                    courseLetters = coursePattern.Match(rawCourse).Groups[1].Value;
+                    courseNumber = coursePattern.Match(rawCourse).Groups[2].Value;
+                    section = coursePattern.Match(rawCourse).Groups[3].Value;
                     title = ws.excelArray[currentRow, 1].Trim();
                     credit = ws.excelArray[currentRow, 2].Trim();
-                    faculty = ws.excelArray[currentRow, 3].Trim();
-                    days = ws.excelArray[currentRow, 4].Trim();
-                    startTime = ws.excelArray[currentRow, 4].Trim();
-                    endTime = ws.excelArray[currentRow, 4].Trim();
+                    rawFaculty = ws.excelArray[currentRow, 3].Trim();
+                    faculty1 = facultyPattern.Match(rawFaculty).Groups[1].Value;
+                    faculty2 = facultyPattern.Match(rawFaculty).Groups[2].Value;
+                    rawTime = ws.excelArray[currentRow, 4].Trim();
+                    day1 = timePattern.Match(rawTime).Groups[1].Value;
+                    day2 = timePattern.Match(rawTime).Groups[2].Value;
+                    day3 = timePattern.Match(rawTime).Groups[3].Value;
+                    day4 = timePattern.Match(rawTime).Groups[4].Value;
+                    startTime = timePattern.Match(rawTime).Groups[5].Value;
+                    startTimeAPM = timePattern.Match(rawTime).Groups[6].Value;
+                    endTime = timePattern.Match(rawTime).Groups[7].Value;
+                    endTimeAPM = timePattern.Match(rawTime).Groups[8].Value;
                     room = ws.excelArray[currentRow, 5].Trim();
 
                     //string outString = courseNumber + " " + section + " " + title + " " + credit + " " + faculty + " " + days + " " + startTime + " " + endTime + " " + room;
-                    string outString = "course letters = " + courseLetters+ " number = " + courseNumber + " section ='" + section + "'";
+                    //string outString = "course letters = " + courseLetters+ " number = " + courseNumber + " section ='" + section + "'";
+                    string outString = "faculty 1 = " + faculty1 + " faculty 2 = " + "'" + faculty2 + "'";
+                    //string outString = "rawCourse = " + rawCourse + " day 1 = " + day1 + " day 2 = " + day2+ " day 3 = " + day3+ " day 4 = " + day4 + " startTime = " + startTime + " startTimeAPM = " + startTimeAPM + " endTime = " + endTime + " endTimeAPM = " + endTimeAPM;
 
 
-                   //                 var match = Regex.Match(ws.excelArray[currentRow, 0], pattern, RegexOptions.IgnoreCase);
-                    
+                    //                 var match = Regex.Match(ws.excelArray[currentRow, 0], pattern, RegexOptions.IgnoreCase);
+
                     //courseNumber = course_Number.Match(ws.excelArray[currentRow, 0], "").Value;
                     //Regex.Replace(ws.excelArray[currentRow, 0], pattern, String.Empty)
                     Console.WriteLine(outString);
