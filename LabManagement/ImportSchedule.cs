df4d266c2f3606f163e4f3e132740514775feb92
Obermeyer <moberme@calstatela.edu>
2019-03-05 16:13:54 -0800

    User import from excel schedule finished

--- LabManagement/ImportSchedule.cs
+++ LabManagement/ImportSchedule.cs
@@ -33,7 +33,7 @@ namespace LabManagement
             Regex facultyPattern = new Regex(@"(\w+)\/?(\w+)?");
             Regex timePattern = new Regex(@"^(TBA|[MTWRFSU])([MTWRFSU]?)([MTWRFSU]?)([MTWRFSU]?)\s(\d{3,4})([AP]?M?)-(\d{3,4})([AP]?M?)");
             Regex roomPattern = new Regex(@"^(ASCB|ASCL|BIOS|ET|FA|HDFC|KH|LACHSA|MUS|PE|SH|ST|TA|TVFM)\s?([A-F]|LH)?(\d{1,4})([A-G])?");
-            string fileName = @"C:\Users\moberme\Documents\LabManagement\ArletteSchedules\fall2019.xlsx";
+            string fileName = @"C:\Users\moberme\Documents\LabManagement\ArletteSchedules\fall2018.xlsx";
             //string fileName = @"C:\Users\moberme\Documents\LabManagement\ArletteSchedules\sum2019.xlsx";
             //string fileName = @"C:\Users\moberme\Documents\LabManagement\ArletteSchedules\ArletteTestSchedule.xlsx";
             ExcelData ws = new ExcelData(fileName, 1);
@@ -51,15 +51,34 @@ namespace LabManagement
                 rawCourse = ws.excelArray[currentRow, 0];
                 isCourse = coursePattern.IsMatch(rawCourse);
 
-                System.Console.WriteLine(" 0 = " +  ws.excelArray[currentRow, 0] + " 1 = " +  ws.excelArray[currentRow, 1] + " 2 = " +  ws.excelArray[currentRow, 2] + " 3 = " +  ws.excelArray[currentRow, 3]);
+                //System.Console.WriteLine(" 0 = " + ws.excelArray[currentRow, 0] + " 1 = " + ws.excelArray[currentRow, 1] + " 2 = " + ws.excelArray[currentRow, 2] + " 3 = " + ws.excelArray[currentRow, 3]);
                 if (isCourse)
                 {
                     title = ws.excelArray[currentRow, 1].Trim();
                     credit = ws.excelArray[currentRow, 2].Trim();
                     Course c = new Course(rawCourse, title, credit);
+
+                    string coarseAndSection = c.Catalog + "-" + c.Section;
+                    User u1, u2;
                     rawFaculty = ws.excelArray[currentRow, 3].Trim();
                     faculty1 = facultyPattern.Match(rawFaculty).Groups[1].Value;
                     faculty2 = facultyPattern.Match(rawFaculty).Groups[2].Value;
+                    bool hasFirstUser = faculty1.Length != 0;
+                    bool hasSecondUser = faculty2.Length != 0;
+                    if (hasFirstUser)
+                    {
+                        u1 = new User(faculty1);
+                    }
+                    else
+                    {
+                        u1 = new User("TBA");
+                    }
+                    if (hasSecondUser)
+                    {
+                        u2 = new User(faculty2);
+                        //Console.WriteLine(coarseAndSection + " u2 = " + u2.Last);
+                    }
+
                     rawTime = ws.excelArray[currentRow, 4].Trim();
                     day1 = timePattern.Match(rawTime).Groups[1].Value;
                     day2 = timePattern.Match(rawTime).Groups[2].Value;
@@ -69,6 +88,7 @@ namespace LabManagement
                     startTimeAPM = timePattern.Match(rawTime).Groups[6].Value;
                     endTime = timePattern.Match(rawTime).Groups[7].Value;
                     endTimeAPM = timePattern.Match(rawTime).Groups[8].Value;
+
                     rawRoom = ws.excelArray[currentRow, 5].Trim();
                     building = roomPattern.Match(rawRoom).Groups[1].Value;
                     roomPrefix = roomPattern.Match(rawRoom).Groups[2].Value;
@@ -86,12 +106,12 @@ namespace LabManagement
 
                     //courseNumber = course_Number.Match(ws.excelArray[currentRow, 0], "").Value;
                     //Regex.Replace(ws.excelArray[currentRow, 0], pattern, String.Empty)
-                 //   Console.WriteLine(outString);
+                    //   Console.WriteLine(outString);
                 }
                 //System.Console.WriteLine("in loop ");
             }
 
-          //Marshal.ReleaseComObject(ws);
+            //Marshal.ReleaseComObject(ws);
             GC.Collect();
             GC.WaitForPendingFinalizers();
             //            System.Environment.Exit(1);
--- LabManagement/User.cs
+++ LabManagement/User.cs
@@ -8,19 +8,35 @@ namespace LabManagement
 {
     internal class User
     {
-        public string userID;
-        public string first;
-        public string last;
-        public string sid;
-        public string email;
-        public string phone;
-        public string cell;
-        public string userType;
-        
+        public int UserID;
+        public string First;
+        public string Last;
+        public string Sid;
+        public string Email;
+        public string Phone;
+        public string Cell;
+        public int UserType;
+
         public User()
         {
 
         }
+
+        public User(string newUser)
+        {
+            Last = newUser;
+            var userTuple = Db.GetTuple(this, "last = '" + Last + "'");
+            bool noUserInDb = userTuple.Count == 0;
+            if (noUserInDb)
+            {
+                UserType = 4;
+                string insertColumns = "last, userTypeFK";
+                string insertData = "'" + Last + "', '" + UserType + "'";
+                UserID = Db.SqlInsert("User", insertColumns, insertData);
+                Console.Write("Inserting User" + insertColumns + " " + insertData + "Returned UserID =" + UserID);
+            }
+        }
+
         public static string getColumnName(int colNumber)
         {
             switch (colNumber)
@@ -36,10 +52,10 @@ namespace LabManagement
 
 
 
-                  
+
             }
-                
+
             return " ";
-        }        
+        }
     }
 }
