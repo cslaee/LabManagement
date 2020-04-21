00356ab981914713dd588e3de63d6ffe5d81d8a1
dakotastream <mike@dakotastream.com>
2020-04-20 17:34:48 -0700

    Changed db schema to auto index EventType. Added EventType class

---
+++ LabManagement/EventType.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabManagement
+{
+    class EventType
+    {
+        public int EventTypeID { get; set; }
+        public string Description { get; set; }
+
+        public EventType(string name)
+        {
+            Description = name;
+            Common.DebugWriteLine(true, "** New EventType with Description = " + Description);
+
+            string[] colname = new[] { "description" };
+            var coldata = new object[] { Description }; 
+            var tuple = Db.GetTuple("EventType", "*", colname, coldata);
+            bool coarseNotInDb = tuple.Count == 0;
+
+            if (coarseNotInDb)
+            {
+                EventTypeID = Db.Insert("EventType", colname, coldata);
+                Common.DebugWriteLine(true, "Description  " + Description + " with EventTypeID = " + EventTypeID + ", now inserted into Db");
+            }
+            else
+            {
+             //   EventTypeID = Convert.ToInt32(tuple[0].ToString());
+                Common.DebugWriteLine(true, "Description " + Description + " with EventTypeID = " + EventTypeID + ", was in Db");
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # LabManagement
-Software to manage Electrical Engineering Laboratories
+Software to manage the Cal State LA Electrical Engineering Laboratories
 
 This is a standalone MS Windows application with no web server capabilities.
+Current implementation
+*Import single page excel class schedules from the EE office and insert them into a SQLite database.
+*Import an excel calendar off of the university website and insert them into the SQLite database.
+*Export the entire SQLite schedule database into a multipage/tab HTML webpage for student viewing.
+ 
